17ec431f456603c7ea775bef61b5689d9a26f6ac
Robert Willemelis <willi84@users.noreply.github.com>
2024-10-14 22:10:41 +0200

    feat: add sample bus data

--- app.py
+++ app.py
@@ -19,7 +19,59 @@ app.add_middleware(
 
 @app.get("/")
 async def home():
-    return JSONResponse({"message": "Hello world!"})
+    return JSONResponse({
+  "busLine": "Bus 5",
+  "route": "Hauptbahnhof - Altona",
+  "totalStations": 5,
+  "stations": [
+    {
+      "stationName": "Hauptbahnhof",
+      "estimatedArrival": "12:05",
+      "checkedPassengers": {
+        "normal": 10,
+        "wheelchair": 1,
+        "elderly": 2
+      }
+    },
+    {
+      "stationName": "Jungfernstieg",
+      "estimatedArrival": "12:12",
+      "checkedPassengers": {
+        "normal": 8,
+        "wheelchair": 0,
+        "elderly": 1
+      }
+    },
+    {
+      "stationName": "Sternschanze",
+      "estimatedArrival": "12:20",
+      "checkedPassengers": {
+        "normal": 15,
+        "wheelchair": 2,
+        "elderly": 3
+      }
+    },
+    {
+      "stationName": "Holstenstraße",
+      "estimatedArrival": "12:27",
+      "checkedPassengers": {
+        "normal": 7,
+        "wheelchair": 0,
+        "elderly": 4
+      }
+    },
+    {
+      "stationName": "Altona",
+      "estimatedArrival": "12:35",
+      "checkedPassengers": {
+        "normal": 5,
+        "wheelchair": 1,
+        "elderly": 2
+      }
+    }
+  ]
+}
+)
 
 
 @app.get("/404")
