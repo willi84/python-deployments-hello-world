5f78061e3931db5b568fec311542c8936f50c449
Robert Willemelis <willi84@users.noreply.github.com>
2024-10-14 22:05:08 +0200

    feat: add fast api middleware

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ Application definition
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow CORS for your localhost (or any other domain you trust)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # Can be a list of origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 async def home():
